db861fd93e049ee6555bcd534e27c8929c7b284d
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-24 14:07:21 +0200

    Add online form

---
+++ cms/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Content
+
+
+class ContentForm(ModelForm):
+    class Meta:
+        model = Content
+        fields = ["git_repository", "file", "collection"]
--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import ContentForm
+
 
 def add_content(request):
-    return HttpResponse("You're looking at")
+    if request.method == "POST":
+        form = ContentForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    else:
+        form = ContentForm()
+
+    return render(request, "add_content.html", {"form": form})
