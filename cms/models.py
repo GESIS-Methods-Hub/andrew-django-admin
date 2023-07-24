661f86c19d79d0c0ab82b493351899ab4c974a5c
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-24 13:30:48 +0200

    Add Collection to data model

--- cms/models.py
+++ cms/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
+class Collection(models.Model):
+    parent_collection = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=200, primary_key=True)
+    abstract = models.TextField()
+
+
 class Content(models.Model):
     git_repository = models.CharField(max_length=200)
     file = models.CharField(max_length=50)
+    collection = models.ManyToManyField(Collection)
+
+    class Meta:
+        unique_together = (
+            "git_repository",
+            "file",
+        )
