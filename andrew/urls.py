f0cbff0fe5eaffaef6e98399d90d136581efef62
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-24 13:50:51 +0200

    Add URL to submit content

--- andrew/urls.py
+++ andrew/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("cms/", include("cms.urls")),
     path("admin/", admin.site.urls),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def add_content(request):
+    return HttpResponse("You're looking at")
