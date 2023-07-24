3f7b0e03e904b8cb578e7d7ad34d4283c3735781
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-07-24 13:30:22 +0200

    Reformat files with Black

--- cms/migrations/0001_initial.py
+++ cms/migrations/0001_initial.py
@@ -4,19 +4,25 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Content',
+            name="Content",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('git_repository', models.CharField(max_length=200)),
-                ('file', models.CharField(max_length=50)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("git_repository", models.CharField(max_length=200)),
+                ("file", models.CharField(max_length=50)),
             ],
         ),
     ]
