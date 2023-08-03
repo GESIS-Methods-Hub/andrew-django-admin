d0bc9a10c9bacda2c6b61d1d4290229c12199c9f
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-08-03 10:20:24 +0200

    Set host for Django database

--- andrew/settings.py
+++ andrew/settings.py
@@ -106,6 +106,7 @@ WSGI_APPLICATION = "andrew.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("ANDREW_DJANGO_ADMIN_POSTGRES_SERVICE", ""),
         "NAME": os.getenv("POSTGRES_DB", "andrew"),
         "USER": os.getenv("POSTGRES_USER", "andrew"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "123456"),
