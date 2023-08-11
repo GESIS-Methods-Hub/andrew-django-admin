c1fdd29fff4a1bdf4ea2552b21d2d202ecbf3923
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-08-11 16:22:34 +0200

    Add filter to serializer
    Based on https://stackoverflow.com/a/28354281/1802726

--- cms/serializers.py
+++ cms/serializers.py
@@ -3,14 +3,26 @@ from rest_framework import serializers
 from .models import Content, Collection
 
 
+class OnlyEnabledFilteredListSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        data = data.filter(enable=True)
+        return super(OnlyEnabledFilteredListSerializer, self).to_representation(data)
+
 class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
         fields = ["git_repository", "filename"]
 
+class OnlyEnabledContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Content
+        fields = ["git_repository", "filename"]
+        list_serializer_class = OnlyEnabledFilteredListSerializer
+
 
 class CollectionSerializer(serializers.ModelSerializer):
-    content_set = ContentSerializer(many=True, read_only=True)
+    content_set = OnlyEnabledContentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Collection
